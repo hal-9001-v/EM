b978a1437619f9823bf30de2b3153b945e39abe2
hal-9001-v <57011035+hal-9001-v@users.noreply.github.com>
2021-06-04 01:00:09 +0200

    Set up

---
+++ Assets/Scripts/PolePositionManager.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Mirror;
+using UnityEngine;
+
+public class PolePositionManager : NetworkBehaviour
+{
+    public int numPlayers;
+    private MyNetworkManager _networkManager;
+
+    private readonly List<PlayerInfo> _players = new List<PlayerInfo>(4);
+    private CircuitController _circuitController;
+    private GameObject[] _debuggingSpheres;
+
+    private void Awake()
+    {
+        if (_networkManager == null) _networkManager = FindObjectOfType<MyNetworkManager>();
+        if (_circuitController == null) _circuitController = FindObjectOfType<CircuitController>();
+
+        _debuggingSpheres = new GameObject[_networkManager.maxConnections];
+        for (int i = 0; i < _networkManager.maxConnections; ++i)
+        {
+            _debuggingSpheres[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            _debuggingSpheres[i].GetComponent<SphereCollider>().enabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (_players.Count == 0)
+            return;
+
+        UpdateRaceProgress();
+    }
+
+    public void AddPlayer(PlayerInfo player)
+    {
+        _players.Add(player);
+    }
+
+    private class PlayerInfoComparer : Comparer<PlayerInfo>
+    {
+        float[] _arcLengths;
+
+        public PlayerInfoComparer(float[] arcLengths)
+        {
+            _arcLengths = arcLengths;
+        }
+
+        public override int Compare(PlayerInfo x, PlayerInfo y)
+        {
+            if (_arcLengths[x.ID] < _arcLengths[y.ID])
+                return 1;
+            else return -1;
+        }
+    }
+
+    public void UpdateRaceProgress()
+    {
+        // Update car arc-lengths
+        float[] arcLengths = new float[_players.Count];
+
+        for (int i = 0; i < _players.Count; ++i)
+        {
+            arcLengths[i] = ComputeCarArcLength(i);
+        }
+        
+        _players.Sort(new PlayerInfoComparer(arcLengths));
+
+        string myRaceOrder = "";
+        foreach (var player in _players)
+        {
+            myRaceOrder += player.Name + " ";
+        }
+
+        Debug.Log("El orden de carrera es: " + myRaceOrder);
+    }
+
+    float ComputeCarArcLength(int id)
+    {
+        // Compute the projection of the car position to the closest circuit 
+        // path segment and accumulate the arc-length along of the car along
+        // the circuit.
+        Vector3 carPos = this._players[id].transform.position;
+
+        int segIdx;
+        float carDist;
+        Vector3 carProj;
+
+        float minArcL =
+            this._circuitController.ComputeClosestPointArcLength(carPos, out segIdx, out carProj, out carDist);
+
+        this._debuggingSpheres[id].transform.position = carProj;
+
+        if (this._players[id].CurrentLap == 0)
+        {
+            minArcL -= _circuitController.CircuitLength;
+        }
+        else
+        {
+            minArcL += _circuitController.CircuitLength *
+                       (_players[id].CurrentLap - 1);
+        }
+
+        return minArcL;
+    }
+}
---
+++ Assets/Scripts/SetupPlayer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Mirror;
+using UnityEngine;
+using Random = System.Random;
+
+/*
+	Documentation: https://mirror-networking.com/docs/Guides/NetworkBehaviour.html
+	API Reference: https://mirror-networking.com/docs/api/Mirror.NetworkBehaviour.html
+*/
+
+public class SetupPlayer : NetworkBehaviour
+{
+    [SyncVar] private int _id;
+    [SyncVar] private string _name;
+
+    private UIManager _uiManager;
+    private MyNetworkManager _networkManager;
+    private PlayerController _playerController;
+    private PlayerInfo _playerInfo;
+    private PolePositionManager _polePositionManager;
+
+    #region Start & Stop Callbacks
+
+    /// <summary>
+    /// This is invoked for NetworkBehaviour objects when they become active on the server.
+    /// <para>This could be triggered by NetworkServer.Listen() for objects in the scene, or by NetworkServer.Spawn() for objects that are dynamically created.</para>
+    /// <para>This will be called for objects on a "host" as well as for object on a dedicated server.</para>
+    /// </summary>
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        _id = NetworkServer.connections.Count - 1;
+    }
+
+    /// <summary>
+    /// Called on every NetworkBehaviour when it is activated on a client.
+    /// <para>Objects on the host have this function called, as there is a local client on the host. The values of SyncVars on object are guaranteed to be initialized correctly with the latest state from the server when this function is called on the client.</para>
+    /// </summary>
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+        _playerInfo.ID = _id;
+        _playerInfo.Name = "Player" + _id;
+        _playerInfo.CurrentLap = 0;
+        _polePositionManager.AddPlayer(_playerInfo);
+    }
+
+    /// <summary>
+    /// Called when the local player object has been set up.
+    /// <para>This happens after OnStartClient(), as it is triggered by an ownership message from the server. This is an appropriate place to activate components or functionality that should only be active for the local player, such as cameras and input.</para>
+    /// </summary>
+    public override void OnStartLocalPlayer()
+    {
+    }
+
+    #endregion
+
+    private void Awake()
+    {
+        _playerInfo = GetComponent<PlayerInfo>();
+        _playerController = GetComponent<PlayerController>();
+        _networkManager = FindObjectOfType<MyNetworkManager>();
+        _polePositionManager = FindObjectOfType<PolePositionManager>();
+        _uiManager = FindObjectOfType<UIManager>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            _playerController.enabled = true;
+            _playerController.OnSpeedChangeEvent += OnSpeedChangeEventHandler;
+            ConfigureCamera();
+        }
+    }
+
+    void OnSpeedChangeEventHandler(float speed)
+    {
+        _uiManager.UpdateSpeed((int) speed * 5); // 5 for visualization purpose (km/h)
+    }
+
+    void ConfigureCamera()
+    {
+        if (Camera.main != null) Camera.main.gameObject.GetComponent<CameraController>().m_Focus = this.gameObject;
+    }
+}
